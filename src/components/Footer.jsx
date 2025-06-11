ee3d91d709986fcef366fced7908c24525ba606c
markoka101 <markoka101@gmail.com>
2025-06-11 14:47:57 -0400

    Footer will show social icons when navbar isn't open

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,17 @@
+import InstagramIcon from './icons/InstagramIcon';
 export default function Footer() {
 	return (
-		<section id="footer" className="relative bottom-0 mb-0 hidden h-[20px] w-full bg-gray-800">
-			<h1 className="text-white">Facts</h1>
+		<section
+			id="footer"
+			className="menuicon-animate fixed bottom-0 z-50 mt-auto flex h-[75px] w-screen items-center justify-end bg-transparent"
+		>
+			<a
+				className="mr-5 inline-flex w-[50px] transform items-center justify-start text-white duration-200 ease-in-out hover:text-pink-500"
+				target="_blank"
+				href="https://www.instagram.com/jair.m3/"
+			>
+				<InstagramIcon className="w-[25px]" />
+			</a>
 		</section>
 	);
 }
