65aa969ff42d1b8bcd64440704d5d52c78171a6d
markoka101 <markoka101@gmail.com>
2025-06-11 14:50:51 -0400

    added the pictures and fixed margins for different screen sizes

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,19 +3,23 @@ import { PortfolioPagePictures } from '../data/PictureData';
 export default function Portfolio() {
 	const pics = PortfolioPagePictures;
 	return (
-		<section id="portfolio" className="flex flex-col items-center justify-center">
-			<header className="mx-auto mb-16 mt-20 flex select-none flex-col text-white font-inter md:mb-40">
+		<section id="portfolio" className="mb-20 flex flex-col items-center justify-center">
+			<header className="mx-auto mb-16 mt-20 flex select-none flex-col text-white font-inter md:mb-32">
 				<h1 className="mb-4 text-7xl font-bold">Portfolio</h1>
 				<p>Scroll down to see some of my work</p>
 			</header>
-			<div className="flex w-full flex-wrap justify-center md:justify-between md:px-[250px]">
+			<div className="flex w-full flex-wrap justify-center px-10 md:justify-between md:px-[70px] lg:px-[100px] 2xl:px-[220px]">
 				{pics.map((p) => {
 					return (
-						<article key={p} className={`${p.ratio} flex w-64`}>
+						<article
+							key={p.alt}
+							className={`${p.ratio} mx-auto my-10 flex w-5/6 items-center justify-center sm:w-64 md:mx-3 md:w-72 lg:w-80 2xl:w-[21.5rem]`}
+						>
 							<img
 								src={p.Path}
 								alt={p.alt}
-								className="h-full w-full object-cover ring-2 ring-gray-300"
+								loading="lazy"
+								className="object-contain ring-2 ring-gray-300"
 							/>
 						</article>
 					);
