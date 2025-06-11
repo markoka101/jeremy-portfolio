119e0716eab41bba66e129011bdfb5a76ab61963
markoka101 <markoka101@gmail.com>
2025-06-11 15:15:10 -0400

    forgot to manually import for prod build

--- src/data/PictureData.js
+++ src/data/PictureData.js
@@ -1,90 +1,59 @@
+// Home Page Images
+import aylinSunset from '../assets/aylin-sunset.JPEG';
+import markCloseupGuitar from '../assets/mark-closeup-guitar.JPG';
+import loungin from '../assets/loungin.JPEG';
+import carMeetupHonda from '../assets/car-meetup-honda.webp';
+
+// Portfolio Images
+import img1 from '../assets/1.JPEG';
+import img2 from '../assets/2.JPEG';
+import img3 from '../assets/3.JPEG';
+import img4 from '../assets/4.jpg';
+import img5 from '../assets/5.jpg';
+import img6 from '../assets/6.jpg';
+import img7 from '../assets/7.jpg';
+import img8 from '../assets/8.JPEG';
+import img9 from '../assets/9.JPEG';
+import img10 from '../assets/10.JPEG';
+import img11 from '../assets/11.JPEG';
+import img12 from '../assets/12.JPG';
+import img13 from '../assets/13.JPG';
+
 export const HomePagePictures = [
 	{
-		Path: '../src/assets/aylin-sunset.JPEG',
+		Path: aylinSunset,
 		alt: 'Portrait in front of sunset',
 		ratio: 'aspect-[1067/1600]'
 	},
 	{
-		Path: '../src/assets/mark-closeup-guitar.JPG',
+		Path: markCloseupGuitar,
 		alt: 'Picture of me (the web developer) shredding on my ESP LTD-EC1000 with EMG 81 80 using Daddario 52-10 gauge strings ;)',
 		ratio: 'aspect-[4/5]'
 	},
 	{
-		Path: '../src/assets/loungin.JPEG',
+		Path: loungin,
 		alt: 'Man lounging in chair',
 		ratio: 'aspect-[1051/1600]'
 	},
 	{
-		Path: '../src/assets/car-meetup-honda.webp',
+		Path: carMeetupHonda,
 		alt: 'Picture of car at an event',
 		ratio: 'aspect-[111/128]'
 	}
 ];
 
 export const PortfolioPagePictures = [
-	{
-		Path: '../src/assets/1.JPEG',
-		alt: 'Shoot in studio leaning on wall',
-		ratio: 'aspect-[2/3]'
-	},
-	{
-		Path: '../src/assets/2.JPEG',
-		alt: 'Outdoor sunset photo shoot',
-		ratio: 'aspect-[1067/1600]'
-	},
-	{
-		Path: '../src/assets/3.JPEG',
-		alt: 'Honda car meetup even photo shoot',
-		ratio: 'aspect-[111/128]'
-	},
-	{
-		Path: '../src/assets/4.jpg',
-		alt: 'Wedding Photo Shoot',
-		ratio: 'aspect-[2249/3200]'
-	},
-	{
-		Path: '../src/assets/5.jpg',
-		alt: 'Bride and groom wedding photos',
-		ratio: 'aspect-[1067/1600]'
-	},
-	{
-		Path: '../src/assets/6.jpg',
-		alt: 'Bride and groom wedding kiss',
-		ratio: 'aspect-[1600/1067]'
-	},
-	{
-		Path: '../src/assets/7.jpg',
-		alt: 'Indoor photo shoot',
-		ratio: 'aspect-[1067/1600]'
-	},
-	{
-		Path: '../src/assets/8.JPEG',
-		alt: 'Picture of car from fron in car meet',
-		ratio: 'aspect-[2717/3200]'
-	},
-	{
-		Path: '../src/assets/9.JPEG',
-		alt: 'Outdoor photo session',
-		ratio: 'aspect-[1067/1600]'
-	},
-	{
-		Path: '../src/assets/10.JPEG',
-		alt: 'Session musician playing guitar',
-		ratio: 'aspect-[663/994]'
-	},
-	{
-		Path: '../src/assets/11.JPEG',
-		alt: 'Studio session pointing at camera',
-		ratio: 'aspect-[663/994]'
-	},
-	{
-		Path: '../src/assets/12.JPG',
-		alt: 'Photo shoot with pet',
-		ratio: 'aspect-[663/994]'
-	},
-	{
-		Path: '../src/assets/13.JPG',
-		alt: 'Outdoor city photo shoot',
-		ratio: 'aspect-[4/5]'
-	}
+	{ Path: img1, alt: 'Shoot in studio leaning on wall', ratio: 'aspect-[2/3]' },
+	{ Path: img2, alt: 'Outdoor sunset photo shoot', ratio: 'aspect-[1067/1600]' },
+	{ Path: img3, alt: 'Honda car meetup even photo shoot', ratio: 'aspect-[111/128]' },
+	{ Path: img4, alt: 'Wedding Photo Shoot', ratio: 'aspect-[2249/3200]' },
+	{ Path: img5, alt: 'Bride and groom wedding photos', ratio: 'aspect-[1067/1600]' },
+	{ Path: img6, alt: 'Bride and groom wedding kiss', ratio: 'aspect-[1600/1067]' },
+	{ Path: img7, alt: 'Indoor photo shoot', ratio: 'aspect-[1067/1600]' },
+	{ Path: img8, alt: 'Picture of car from fron in car meet', ratio: 'aspect-[2717/3200]' },
+	{ Path: img9, alt: 'Outdoor photo session', ratio: 'aspect-[1067/1600]' },
+	{ Path: img10, alt: 'Session musician playing guitar', ratio: 'aspect-[663/994]' },
+	{ Path: img11, alt: 'Studio session pointing at camera', ratio: 'aspect-[663/994]' },
+	{ Path: img12, alt: 'Photo shoot with pet', ratio: 'aspect-[663/994]' },
+	{ Path: img13, alt: 'Outdoor city photo shoot', ratio: 'aspect-[4/5]' }
 ];
