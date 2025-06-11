2da47547e5c33da722697ed46cf4bf78b97a3c85
markoka101 <markoka101@gmail.com>
2025-06-11 14:46:54 -0400

    added paragraph and proper image

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,9 +3,9 @@ export default function About() {
 	return (
 		<section
 			id="about"
-			className="flex min-h-screen w-full flex-1 flex-col items-start md:flex-row"
+			className="flex min-h-screen w-full flex-1 flex-col items-start pb-24 md:flex-row"
 		>
-			<div className="mt-20 flex w-full select-none flex-col px-12 text-white font-inter md:w-1/2">
+			<div className="mt-20 flex w-full flex-col px-10 text-white font-inter sm:px-12 lg:w-1/2">
 				<div className="mb-5 flex flex-col text-7xl font-bold md:flex-row">
 					<h1>About{'\u00A0'} </h1>
 					<h1 className="ml-14 md:ml-0"> Jeremy</h1>
@@ -16,22 +16,24 @@ export default function About() {
 					gapBetweenLine={1}
 					lineThicknessGrowth={0.2}
 					lineColor="bg-white"
-					customStyles="mb-5 w-full md:w-[480px]"
+					customStyles="mb-5 w-full lg:w-[480px]"
 				/>
 				<p className="md:text-lg">
-					Hi, I’m a Goof, a passionate freelance photographer with an eye for capturing life’s most
-					memorable moments. Whether it’s portraits, events, or landscapes, I strive to tell unique
-					stories through my lens. With a creative approach and attention to detail, I aim to
-					deliver images that inspire and connect.When I’m not behind the camera, you’ll find me
-					exploring new places, seeking out natural light, and always looking for the next perfect
-					shot. Let’s work together to create something beautiful!This is a placeholder bio
+					Hi, I’m Jeremy Messina— a photographer with a deep love for capturing real moments, honest
+					emotions, and the beauty in the everyday. My journey behind the lens started with a
+					curiosity for light and storytelling, and over the years, it’s grown into a full-blown
+					passion. Whether I’m documenting a quiet moment between loved ones, the energy of a city
+					street, or the subtle details that often go unnoticed, my goal is always the same: to
+					create images that feel personal, timeless, and true. When I’m not behind the camera, you
+					can usually find me at a trendy restaurant or hanging out with my friends. Thanks for
+					being here — I’m glad that you are.
 				</p>
 			</div>
 
-			<aside className="my-auto mt-52 hidden h-full w-1/2 justify-center pr-20 md:flex">
-				<div className="aspect-[9/16] w-80 outline-double outline-4 outline-offset-4 outline-white">
+			<aside className="my-auto mt-[11.2rem] hidden h-full w-1/2 justify-center pr-20 lg:flex">
+				<div className="aspect-[2/3] w-96 outline-double outline-4 outline-offset-2 outline-white">
 					<img
-						src="../src/assets/jair-bouta-die-xd.png"
+						src="../src/assets/jeremy-pic.jpg"
 						alt="Portrait of Jeremy Messina"
 						className="h-full w-full object-cover"
 					/>
