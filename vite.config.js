9e5219d2129be4180920267478a7f22b1390e386
markoka101 <markoka101@gmail.com>
2025-06-13 16:36:05 -0400

    started configuration for seo

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import sitemapPlugin from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+	plugins: [
+		react(),
+		sitemapPlugin({
+			hostname: 'https://jeremy-photography-portfolio.netlify.app/',
+			dynamicRoutes: ['/', '/about-jeremy', '/portfolio', '/book-photo-session', '/session-pricing']
+		})
+	]
 });
